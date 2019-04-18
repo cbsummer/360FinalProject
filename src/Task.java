4f6ebf07100267be4904e6d12713ca94e5c31c7f
chase <chase@DESKTOP-97K2RNU.3521.dhcp.asu.edu>
2019-04-17 17:19:03 -0700

    Added Function class and Task object

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,17 @@
 
 public class Task {
 
+	private int priority;
+	private String description;
+	private String dueDate;
+	private int status;
+	
+	private String startDate;
+	private String endDate;
+	
+	enum status{
+		
+		NotStarted, InProgress, Finished;
+		
+	}
 }
--- src/ToDoListFunctions.java
+++ src/ToDoListFunctions.java
@@ -1,9 +1,17 @@
 
 public class ToDoListFunctions {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void PrioritySort(){
+		
+		
+		
 	}
+	
+	Object task;
+	
 
 }
+
+
+
+
