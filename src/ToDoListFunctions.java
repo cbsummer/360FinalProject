7eedefa58a5e9b2c763f977e3452a8e7f5181f57
Andrew Jarman <31833701+atjarman@users.noreply.github.com>
2019-04-19 09:43:16 -0700

    Took the previously added functions out of the PrioritySort functions

--- src/ToDoListFunctions.java
+++ src/ToDoListFunctions.java
@@ -1,39 +1,38 @@
 
 public class ToDoListFunctions {
 
-	public void PrioritySort(){
-				
-		
-		public void add() {
-			
-		}
-		
-		public void delete() {
-					
-		}
-		
-		public void modify() {
-			
-		}
-		
-		public void save() {
-			
-		}
-		
-		public load() {
-			
-		}
-		
-		public print() {
-			
-		}
-		
-		public void sort() {
-			
-		}
 
+	public void add() {
 		
+	}
+	
+	public void delete() {
+				
+	}
+	
+	public void modify() {
 		
+	}
+	
+	public void save() {
+		
+	}
+	
+	public load() {
+		
+	}
+	
+	public print() {
+		
+	}
+	
+	public void sort() {
+		
+	}
+
+	
+	public void PrioritySort(){
+						
 	}
 	
 	Object task;
