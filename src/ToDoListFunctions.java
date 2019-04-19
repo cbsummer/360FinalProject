ab06fd254d6365e0b61c8558b4a37e47bb56c9ba
Andrew Jarman <31833701+atjarman@users.noreply.github.com>
2019-04-19 09:38:04 -0700

    Added some needed function headers

--- src/ToDoListFunctions.java
+++ src/ToDoListFunctions.java
@@ -2,17 +2,37 @@
 public class ToDoListFunctions {
 
 	public void PrioritySort(){
+				
 		
-		/*
-		 * 
-		 * insert
-		 * delete
-		 * modify
-		 * save
-		 * load
-		 * print
-		 * 
-		 */
+		public void add() {
+			
+		}
+		
+		public void delete() {
+					
+		}
+		
+		public void modify() {
+			
+		}
+		
+		public void save() {
+			
+		}
+		
+		public load() {
+			
+		}
+		
+		public print() {
+			
+		}
+		
+		public void sort() {
+			
+		}
+
+		
 		
 	}
 	
